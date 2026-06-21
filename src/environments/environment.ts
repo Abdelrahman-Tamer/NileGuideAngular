64ecce411a7a2422d3974e825ca17d4ab72f3873
Abdelrahman Emam <botta12779@gmail.com>
2026-06-21 05:20:52 +0200

    Add Google Maps API key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  googleMapsApiKey: '',
+  googleMapsApiKey: 'AIzaSyBanWR8k5Y_z_dZgwPiw2Uhmzz6IQid0Cw',
   emailJs: {
     serviceId: '',
     templateId: '',
